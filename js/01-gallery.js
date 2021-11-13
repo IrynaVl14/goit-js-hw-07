66c5c6f0edf86f4c20b0009c99a2a7ec9d3f34f1
Iryna Vlasenko <via-lilly@ukr.net>
2021-11-13 20:03:30 +0200

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,53 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const makeListOfImages = ({ preview, original, description }) => {
+  return `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+        </div>
+  `    
+};
+
+const listOfImages = galleryItems.map(makeListOfImages).join('');
+const galleryEl = document.querySelector('div.gallery');
+galleryEl.insertAdjacentHTML('afterbegin', listOfImages);
+
+galleryEl.addEventListener('click', onGalleryClick)
+
+function onGalleryClick(evt) {
+    evt.preventDefault();
+    const isImgEl = evt.target.classList.contains('gallery__image');
+
+    if (!isImgEl) {
+        return;
+    }
+    
+    const instance = basicLightbox.create(`
+	<img
+        width="1400"
+        height="900"
+        src="${evt.target.dataset.source}"            
+    />
+`)
+    instance.show()
+
+    galleryEl.addEventListener("keydown", evt => {
+if (evt.code==='Escape') {
+    instance.close()
+}
+});
+}
+
+
+
+
+
